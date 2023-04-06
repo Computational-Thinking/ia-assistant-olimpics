23d78788d84eddcad413c2be4b7641aa3adc807e
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-04-06 12:22:47 +0100

    workflow

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -1,7 +1,7 @@
 const { description } = require('../../package')
 
 module.exports = {
-  base: '',
+  base: '', // '/ia-assistant-olimpics/',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
